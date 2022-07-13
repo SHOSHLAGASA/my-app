2605f8f6ba1043b0146ecb45a232a862a0e174a4
shoshana lagasa <shoshanalagasa850@gmail.com>
2022-07-12 19:39:02 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = ({title}) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    title: 'Tickets to Shtetl Band', 
+}
+export default Header
