8f12590082e660111ac052f6e852086a9fbbee8d
shoshana lagasa <shoshanalagasa850@gmail.com>
2022-07-13 21:33:41 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -12,16 +12,3 @@ function App() {
 }
 
 export default App;
-
-// const Header = ({title}) => {
-//   return (
-//     <header>
-//       <h1>{title}</h1>
-//     </header>
-//   )
-// }
-
-// Header.defaultProps = {
-//     title: 'Tickets to Shtetl Band', 
-// }
-// export default Header
