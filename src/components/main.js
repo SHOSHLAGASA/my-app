8cfec1142bd7265631768ea8af819f81b2c55e7b
shoshana lagasa <shoshanalagasa850@gmail.com>
2022-08-01 18:27:41 -0500

    SPA

--- src/components/main.js
+++ src/components/main.js
@@ -1,56 +1,36 @@
-// import React,  { useState } from "react"
+import React,  { useState } from "react"
 
-import { Route, NavLink, HashRouter} from "react-router-dom"; 
-import Home from "./views/Home"
-import Results from "./views/Results"
-import CheckOut from "./views/CheckOut"
+ import Home from "./views/Home"
+ import Results from "./views/Results"
+ import CheckOut from "./views/CheckOut"
+
 
 
 const Main = () => {
-    return (
-        <HashRouter>
-            <div>
-<ul className="pages">
-    <li><NavLink to="/">Home</NavLink></li>
-    <li><NavLink to="/Results">Results</NavLink></li>
-    <li><NavLink to="/CheckOut">CheckOut</NavLink></li>
-</ul>
-<div>
-    <Route path="/" views={Home}/>
-    <Route path="/Results" views={Results}/>
-    <Route path="/CheckOut" views={CheckOut}/>
+const [tickets, setTickets] = useState(0);
+const [showResults, setShowResults] = useState(false)
 
-</div>
-</div>
+const purchase = () => {
+    setTickets(tickets + 1);
+ if(tickets === 10){
+sold()
+ }  
+}
+ function sold() { 
+     alert('All tickets are sold out');
 
-        </HashRouter>
+ }
+ return (
+        <main>
+            <div className="Box">
+            <p>Show in St.Louis MO it August 4  </p>
+            <button onClick={purchase}>purchase</button>
+            {tickets}
+            if(showResults === )
+            </div> 
+        </main>
     )
     }
-    
-
-// const Main = () => {
-// const [tickets, setTickets] = useState(0);
-
-// const purchase = () => {
-//     setTickets(tickets + 1);
-//  if(tickets === 10){
-// sold()
-//  }  
-// }
-//  function sold() { 
-//      alert('All tickets are sold out');
-
-//  }
-//  return (
-//         <main>
-//             <div className="Box">
-//             <p>Show in St.Louis MO it August 4  </p>
-//             <button onClick={purchase}>purchase</button>
-//             {tickets}
-//             </div> 
-//         </main>
-//     )
-//     }
 
     
 
